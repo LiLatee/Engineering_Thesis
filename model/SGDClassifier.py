7d21be3ae9366e057302a93c0a35417e963da59d
lilatee <marcin.hradowicz@gmail.com>
2019-08-06 15:49:06 +0200

    transform_df_to_df_with_one_one_vectors

--- model/SGDClassifier.py
+++ model/SGDClassifier.py
@@ -3,10 +3,10 @@ import numpy as np
 import pickle
 import os
 import json
+import collections
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 
@@ -58,8 +58,8 @@ class model_SGDClassifier:
 
         return df
 
-    def transform_df_into_df_with_one_hot_vectors(self, df_to_transform):
-        required_column_names_list = self.read_requred_column_names()
+    def transform_df_into_df_with_one_hot_vectors(self, df_to_transform: pd.DataFrame) -> pd.DataFrame:
+        required_column_names_list = self.read_required_column_names()
         data_as_dict = json.loads(df_to_transform.T.to_json())
 
         samples = []
@@ -84,21 +84,21 @@ class model_SGDClassifier:
 
         return df
 
-    def create_model_2(self, json_data):
+    def create_model_2(self, json_data: str) -> None:
         self.df_original = pd.read_json(json_data)
 
         df_one_hot_vectors = self.transform_df_into_df_with_one_hot_vectors(self.df_original)
         X = df_one_hot_vectors.iloc[:, 3:].values
         y = df_one_hot_vectors.iloc[:, :1].values.ravel()  # tutaj powinny być chyba 3 kolumny
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-        print('Liczba etykiet w zbiorze y:', np.bincount(y))
-        print('Liczba etykiet w zbiorze y_train:', np.bincount(y_train))
-        print('Liczba etykiet w zbiorze y_test:', np.bincount(y_test))
+        # print('Liczba etykiet w zbiorze y:', np.bincount(y))
+        # print('Liczba etykiet w zbiorze y_train:', np.bincount(y_train))
+        # print('Liczba etykiet w zbiorze y_test:', np.bincount(y_test))
 
-        sc = StandardScaler()
-        sc.fit(X_train)
-        X_train_std = sc.transform(X_train)
-        X_test_std = sc.transform(X_test)
+        self.sc = StandardScaler()
+        self.sc.fit(X_train)
+        X_train_std = self.sc.transform(X_train)
+        X_test_std = self.sc.transform(X_test)
 
         # ppn = Perceptron(eta0=0.1, random_state=1, n_jobs=-1)
         # ppn.fit(X_train_std, y_train)
@@ -110,7 +110,10 @@ class model_SGDClassifier:
         lr = SGDClassifier(loss='log', verbose=0, n_jobs=-1, random_state=1)
         lr.fit(X_train_std, y_train)
 
+        print(collections.Counter(y_test))
         y_pred = lr.predict(X_test_std)
+        print(collections.Counter(y_pred))
+
         print('Nieprawidłowo sklasyfikowane próbki: %d' % (y_test != y_pred).sum())
         print('Dokładność: %.2f' % lr.score(X_test_std, y_test))
 
@@ -148,15 +151,15 @@ class model_SGDClassifier:
         print('Nieprawidłowo sklasyfikowane próbki: %d' % (y_test != y_pred).sum())
         print('Dokładność: %.2f' % lr.score(X_test_std, y_test))
 
-    def update_model(self):
+    def update_model(self) -> None:
         pass
 
-    def read_requred_column_names(self):
-        required_column_name_file = open('model/required_column_names_list.txt', 'r')
+    def read_required_column_names(self) -> list:
+        required_column_name_file = open('required_column_names_list.txt', 'r')
         required_column_names_list = required_column_name_file.read().splitlines()
         return required_column_names_list
 
-    def remove_nones_in_dict(self, dictionary):
+    def remove_nones_in_dict(self, dictionary: dict):
         result = {}
         for k, v in dictionary.items():
             if v is None:
@@ -165,8 +168,8 @@ class model_SGDClassifier:
                 result[k] = v
         return result
 
-    def transform_one_row_in_one_hot_vectors_row(self, row_as_json):
-        required_column_names_list = self.read_requred_column_names()
+    def transform_one_row_in_one_hot_vectors_row(self, row_as_json: json) -> list:
+        required_column_names_list = self.read_required_column_names()
 
         row_as_dict = json.loads(row_as_json)
         transformed_row_as_dict = dict.fromkeys(required_column_names_list)
@@ -174,8 +177,8 @@ class model_SGDClassifier:
         transformed_row_as_dict['Sale'] = row_as_dict['Sale']
         transformed_row_as_dict['SalesAmountInEuro'] = row_as_dict['SalesAmountInEuro']
         transformed_row_as_dict['time_delay_for_conversion'] = row_as_dict['time_delay_for_conversion']
-        transformed_row_as_dict['click_timestamp'] = row_as_dict['click_timestamp']
-        transformed_row_as_dict['nb_clicks_1week'] = row_as_dict['nb_clicks_1week']
+        transformed_row_as_dict['click_timestamp'] = int(row_as_dict['click_timestamp'])
+        transformed_row_as_dict['nb_clicks_1week'] = int(row_as_dict['nb_clicks_1week'])
 
         for column_name, value in row_as_dict.items():
             transformed_column_name = column_name + '_' + str(value)
@@ -186,11 +189,14 @@ class model_SGDClassifier:
 
         return list(transformed_row_as_dict.values())
 
-    def predict(self, X):
-        transformed_X = self.transform_one_row_in_one_hot_vectors_row(X)
-        transformed_X = transformed_X[3:] # remove sales features from sample
-        y = self.model.predict_proba([transformed_X])
-        return y
+    def predict(self, x: json):
+        transformed_x = self.transform_one_row_in_one_hot_vectors_row(x)
+        transformed_x = transformed_x[3:] # remove sales features from sample
+        transformed_x = self.sc.transform([transformed_x])
+        probability = self.model.predict_proba(transformed_x)
+        y = self.model.predict(transformed_x)
+
+        return y, probability
 
 
     def save_model(self):
@@ -209,11 +215,45 @@ class model_SGDClassifier:
         self.model = pickle.load(open(os.path.join(current_dir, 'pickle_objects', 'SGDClassifier.pkl'), mode='rb'))
         print("LOG: " + "model load from directory: " + current_dir + '\SGDClassifier.pkl')
 
+    def test(self):
+        headers = ['Sale', 'SalesAmountInEuro', 'time_delay_for_conversion', 'click_timestamp', 'nb_clicks_1week',
+                   'product_price', 'product_age_group', 'device_type', 'audience_id', 'product_gender',
+                   'product_brand', 'product_category(1)', 'product_category(2)', 'product_category(3)',
+                   'product_category(4)', 'product_category(5)', 'product_category(6)', 'product_category(7)',
+                   'product_country', 'product_id', 'product_title', 'partner_id', 'user_id']
+        df = pd.read_csv('D:\Projekty\Engineering_Thesis\Dataset\Criteo_Conversion_Search\CriteoSearchData-sorted.csv',
+                         sep='\t',
+                         nrows=100000,
+                         names=headers,
+                         low_memory=False,
+                         usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22])
 
+        training_data_json = df[10000:100000].to_json()
+
+        print("Training...")
+        self.create_model_2(training_data_json)
+        print("DONE")
+        print("Testing...")
+        f = np.array([])
+        for id, row in df[1:1001].iterrows():
+            y, prob = self.predict(row.to_json())
+            f = np.append(f, y)
+
+        print(collections.Counter(f))
+
+
+
+        # tests = []
+        # for id, row in df[1001:2000].iterrows():
+        #     transformed_x = self.transform_one_row_in_one_hot_vectors_row(row.to_json())
+        #     transformed_x = transformed_x[3:]  # remove sales features from sample
+        #     tests.append(transformed_x)
+        # tests = np.asarray(tests)
+        # tests = self.sc.transform(tests)
+        # y = self.model.predict(tests)
+        # print("y: %s\t probability: %s" % (str(y), str(444)))
 
 if __name__ == '__main__':
     m = model_SGDClassifier()
-    m.create_model(rows=10000, filepath='D:\Projekty\Engineering_Thesis\Dataset\Criteo_Conversion_Search\CriteoSearchData.csv')
-    m.save_model()
-    m.predict(None)
+    m.test()
 
