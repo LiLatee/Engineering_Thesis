bc232fc3f10e730183b30bb6c33ab348e878c5ec
LiLatee <marcin.hradowicz@gmail.com>
2019-08-12 10:28:59 +0200

    - docker container names changed - dataset directory changed

--- prediction_server/tasks.py
+++ prediction_server/tasks.py
@@ -10,5 +10,5 @@ def update_model(data):
     print("Updating model asynchronously...")
     time.sleep(1)
     print("Done updating model.")
-    requests.request(method='GET', url='http://engineeringthesis_prediction_server_1:5000/update_ready', data='new_model_file_name.pkl')
+    requests.request(method='GET', url='http://engineering_thesis_prediction_server_1:5000/update_ready', data='new_model_file_name.pkl')
     return None
