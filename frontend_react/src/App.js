84c7f880d98625fca7d591c402a116043456746e
ngiersz <ngiersz@gmail.com>
2019-11-19 23:12:27 +0100

    WIP react frontend (not working)

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -14,31 +14,53 @@ class App extends Component {
 
   componentDidMount() {
     this.ws_data_provider.onopen = () => {
-    // on connecting, do nothing but log it to the console
     console.log('WS to data_provider: connected.')
     }
 
     this.ws_evaluation_server.onopen = () => {
-      // on connecting, do nothing but log it to the console
     console.log('WS to evaluation_server: connected.')
     }
 
     this.ws_evaluation_server.onmessage = evt => {
-    // listen to data sent from the websocket server
     const message = JSON.parse(evt.data)
-    console.log(evt.data);
-    this.setState({models: message})
-    console.log('After setState: ' + this.state);
-    // for(var key in message) {
-    //   if(message.hasOwnProperty(key)) {
-    //       var model = message[key];
-    //       console.log(model);
-    //       var correct_prediction_ratio = model["correct_predictions"] / model["processed_samples"];
-    //       console.log('correct_prediction_ratio = ' + correct_prediction_ratio);
-    //       var id = model['id'];
-    //       this.setState();
-    //   }
-    // }    
+    // console.log(evt.data);
+
+    var new_state = Array([]);
+    for(var key in message) {
+      if(message.hasOwnProperty(key)) {  
+          var model = message[key];
+          // for(var key2 in model) {
+          //   console.log(key2);
+          //   if(key2 == 'x') {
+          //     console.log(key2 + " -> " + model[key2]);
+          //   }
+          // }
+          // console.log(model);
+          // var correct_prediction_ratio = model["correct_predictions"] / model["processed_samples"];
+          // console.log('correct_prediction_ratio = ' + correct_prediction_ratio);
+          // console.log("message[key].hasOwnProperty('x')=" + message[key].hasOwnProperty('x'));
+          // if(message[key].hasOwnProperty('x')) {
+          //   console.log("PUSH TO ARRAY");
+          //   message[key]['y'].push(correct_prediction_ratio);
+          //   message[key]['x'].push(message[key]['x'].length);
+          // } else {
+          //   console.log("ADD AN ARRAY");
+          //   message[key]['y'] = Array([correct_prediction_ratio]);
+          //   message[key]['x'] = Array([model["processed_samples"]]);
+          // }
+
+          // console.log("x="+message[key]['x']);
+          // console.log("y="+message[key]['y']);
+          var new_model_state = this.addPoint(model);
+          console.log("New model state = " + new_model_state.x);
+          new_state.push(new_model_state);
+      }
+    }   
+    console.log("Message: " + message);
+    this.setState({models: new_state});
+    // this.setState({models: message})
+    // console.log('After setState: ' + JSON.parse(this.state));
+ 
 
     this.ws_data_provider.onclose = () => {
       console.log('WS to data_provider: disconnected.')
@@ -57,6 +79,22 @@ class App extends Component {
     this.ws_evaluation_server.send('start');
   }
 
+  // addPoint = () => {
+    addPoint = (model) => {
+    console.log("addPoint");
+    var correct_prediction_ratio = model["correct_predictions"] / model["processed_samples"];
+    if(model.hasOwnProperty('x')) {
+      console.log("YES model.hasOwnProperty('x')");
+      model['y'].push(correct_prediction_ratio);
+      model['x'].push(model['processed_samples']);
+    } else {
+      console.log("NO model.hasOwnProperty('x')");
+      model['y'] = Array([correct_prediction_ratio]);
+      model['x'] = Array([model["processed_samples"]]);
+    }
+    return model;
+  }
+
 
   render() {
     return (
@@ -64,10 +102,11 @@ class App extends Component {
           <div websocket={this.ws_data_provider} />
           <Header />
           <button onClick={this.sendStartMessage}>Start</button>
-          <Models models={this.state.models} />
+          <Models models={this.state.models} addPoint={this.addPoint}/>
       </div>
     );
   }
 }
 
 export default App;
+
